9daff8e9840381dbf0b172f7fbfb0137f1632cb3
Clayton Allen <claytonallen@Claytons-MacBook-Pro.local>
2017-09-03 22:39:36 -0500

    Added a stubbed out version of main aite navigation

--- src/app/admin/manage-cta/manage-cta.component.ts
+++ src/app/admin/manage-cta/manage-cta.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-	templateUrl: './manage-cta.component.html'
+	templateUrl: './manage-cta.component.html',
+	styleUrls: ['./manage-cta.component.css']
 })
 export class ManagaCTAComponent{}
